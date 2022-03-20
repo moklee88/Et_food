3e364bd2e481b167125cfaebf7d6754a1fb68384
moklee88 <cuihaomok88@gmail.com>
2022-03-21 00:54:39 +0800

    Done Navigation drawer

--- app/src/main/java/com/example/etfood/MainActivity.kt
+++ app/src/main/java/com/example/etfood/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.etfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.etfood.databinding.ActivityMainBinding
+import com.example.etfood.databinding.ActivityMenuBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnStart.setOnClickListener{ goMenu()}
+    }
+
+    private fun goMenu() {
+        val intent = Intent(this, Menu::class.java)
+
+        startActivity(intent)
     }
 }
